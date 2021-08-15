3b7e7904f8c50fba550783bf4a6f74f56831dc89
heomeco2014 <45783472+heomeco2014@users.noreply.github.com>
2021-08-15 17:41:41 +0700

    Add files via upload

---
+++ Client.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiServer
+{
+    class Client
+    {
+        //Info
+        public string username;
+        //Socket
+        public const int BUFFER_SIZE = 2048;
+        public byte[] buffer = new byte[BUFFER_SIZE];
+        public Socket socket;
+    }
+    class Info
+    {
+        public string username;
+        public string password;
+        public string date;
+        public string fullName;
+        public string note;
+        public int point;
+    }
+}
